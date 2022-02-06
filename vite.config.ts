b7cb7d9a1e9fca36a5e25043d1cbd777dd59241b
adrian <adry.dabp@gmail.com>
2022-02-06 13:58:13 -0400

    Cambios

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/': {
-        target: 'https://cursos-edteam.github.io/react-ts/'
-      },
-
-      '/detail/:id': {
-        target: 'https://cursos-edteam.github.io/react-ts/detail/2'
-      }
-    }
-  },
+  base: '/react-ts/',
   plugins: [react()]
 })
